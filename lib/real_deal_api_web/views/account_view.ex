7aa8cdabdfca2a81ddb135f119cda1062649fe2a
richie <connecttorichie@gmail.com>
2024-07-10 01:22:05 +0530

    Initial commit.

---
+++ lib/real_deal_api_web/views/account_view.ex
@@ -0,0 +1,19 @@
+defmodule RealDealApiWeb.AccountView do
+  use RealDealApiWeb, :view
+  alias RealDealApiWeb.AccountView
+
+  def render("index.json", %{accounts: accounts}) do
+    %{data: render_many(accounts, AccountView, "account.json")}
+  end
+
+  def render("show.json", %{account: account}) do
+    %{data: render_one(account, AccountView, "account.json")}
+  end
+
+  def render("account.json", %{account: account}) do
+    %{
+      id: account.id,
+      email: account.email
+    }
+  end
+end
